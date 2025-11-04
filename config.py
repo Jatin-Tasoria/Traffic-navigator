651565b52ffa8b2686b712913b2b9d0d951752b2
Jatin Tasoria <tasoriaj4@gmail.com>
2025-11-04 13:14:08 +0530

    Update database password in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_connection():
     return psycopg2.connect(
         dbname="traffic_db",
         user="postgres",
-        password="Jatin",
+        password="your_password",
         host="localhost",
         port="5432"
     )
